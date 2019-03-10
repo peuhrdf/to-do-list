fe5c2c08f7c50d882c905f2da1b08a651bb1f02f
Pedro <pedrohrdf.ti@gmail.com>
2019-03-10 10:15:41 -0300

    add python decouple

--- to_do/settings.py
+++ to_do/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,16 +78,11 @@ WSGI_APPLICATION = 'to_do.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'todo',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 

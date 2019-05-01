ecf5e37f1bd351653236099961218fd59f9e7832
Pedro <pedrohrdf.ti@gmail.com>
2019-04-30 21:56:34 -0300

    statics

--- to_do/settings.py
+++ to_do/settings.py
@@ -136,3 +136,9 @@ PUBLIC_VIEWS = [
     'django.contrib.auth.views.LoginView',
     'django.contrib.auth.views.LogoutView'
 ]
+
+STATIC_URL ='/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL  = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
